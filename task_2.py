288ebbab703cfc1e420fd089acf8acd7e76a194c
alekanic <koirallabublik@gmail.com>
2024-06-13 22:52:03 +0300

    Добавила второе задание

---
+++ task_2.py
@@ -0,0 +1,22 @@
+print("Введите первый этап развития человека: ")
+step_1 = input()
+# Homo
+print("Введите следующий этап развития человека: ")
+step_2 = input()
+# Homo habilis
+print("Введите следующий этап развития человека: ")
+step_3 = input()
+# Homo erectus
+print("Введите следующий этап развития человека: ")
+step_4 = input()
+# Homo heidelbergensis
+print("Введите следующий этап развития человека: ")
+step_5 = input()
+# Homo Neandersovans
+print("Введите следующий этап развития человека: ")
+step_6 = input()
+# Homo sapiens
+print("Введите следующий этап развития человека: ")
+step_7 = input()
+# Homo sapiens sapiens
+print(step_1, step_2, step_3, step_4, step_5, step_6, step_7, sep="=>")
