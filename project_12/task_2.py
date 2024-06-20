828515b3261e2eaea2a504d07e3f7a0731a9c3f2
alekanic <koirallabublik@gmail.com>
2024-06-20 12:26:54 +0300

    Добавила 12-ое задание

---
+++ project_12/task_2.py
@@ -0,0 +1,17 @@
+class Transport:
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"Вместимость одного автобуса {self.name}: {capacity} пассажиров"
+
+
+class Autobus(Transport):
+    def method(self):
+        print()
+
+child = Autobus("Renault Logan", 180, 12)
+print(child.seating_capacity(50))
