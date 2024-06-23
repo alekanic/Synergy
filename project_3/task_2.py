bdd6fb421c8108404c718d3f82b2b675ecb2f0ec
alekanic <koirallabublik@gmail.com>
2024-06-23 09:43:25 +0300

    Поправила третье задание

--- project_3/task_2.py
+++ project_3/task_2.py
@@ -1,3 +1,19 @@
+def count_letter(word):
+    count_a = word.count('a')
+    count_e = word.count('e')
+    count_i = word.count('i')
+    count_o = word.count('o')
+    count_u = word.count('u')
+
+    tmp_list = {'a':count_a, 'e':count_e, 'i':count_i, 'o':count_o, 'u':count_u}
+
+    for i in tmp_list.keys():
+        if tmp_list[i] == 0:
+            print(f"Гласная \'{i}\': False")
+        else:
+            print(f"Гласная \'{i}\': встречается в слове {tmp_list[i]} раз(а)")
+
+
 print("Введите слово: ")
 word = input()
 vowels = set("aeiou")
@@ -10,5 +26,8 @@ for letter in word:
     else:
         consonants_count += 1
 
+
+
 print("Количество гласных: " + str(vowels_count))
 print("Количество согласных: " + str(consonants_count))
+count_letter(word)
