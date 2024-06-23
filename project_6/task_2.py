a59044164171c3549378949ba6455fbb31c6a58a
alekanic <koirallabublik@gmail.com>
2024-06-23 09:58:29 +0300

    Добавила текст задания и исправила еще одну домашку

--- project_6/task_2.py
+++ project_6/task_2.py
@@ -1,13 +1,11 @@
 print("Введите число N (количество чисел в строке): ")
 number = int(input())
 print("Введите через пробел N чисел")
-tmp = input().split()
-arr = []
-arr.append(tmp[-1])
-for i in range(1, number - 1):
-    arr.append(tmp[i + 1])
-arr.append(tmp[0])
+tmp = list(input().split())
+
 print("Массив, который вы ввели: ")
 print(*tmp)
 print("Измененный массив: ")
-print(*arr)
+print(*tmp[-1], end='')
+print(' ', end='')
+print(*tmp[0:-1:])
